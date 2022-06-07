04889799d87557c583c3dacfaa553d1ad714cff2
ultimatelite <89692490+ultimatelite@users.noreply.github.com>
2022-06-07 07:09:16 +0700

    Create Main.java

---
+++ src/com/project/CRUD/Main.java
@@ -0,0 +1,17 @@
+package com.project.CRUD;
+
+/*
+
+ Java data
+
+ Compiler: Java SE 17
+ Text Editor: IntelliJ IDEA
+
+*/
+
+public class Main {
+
+    public static void main(String[] args){
+        new MainFunctions().getTable();
+    }
+}
